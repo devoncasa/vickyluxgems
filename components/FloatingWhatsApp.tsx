41794ca061b7328014094ba69ec0a81bb958741e
vickyamber <k.laohasiri@gmail.com>
2026-06-07 22:29:53 +0700

    feat: add floating WhatsApp contact button

---
+++ components/FloatingWhatsApp.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FloatingWhatsApp: React.FC = () => {
+  return (
+    <a
+      href="https://wa.me/66631959922?text=Hello%20Vicky%20LuxGems%2C%20I%20would%20like%20to%20ask%20about%20your%20gemstones."
+      className="whatsapp-float"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Chat with Vicky LuxGems on WhatsApp"
+    >
+      <svg width="28" height="28" viewBox="0 0 32 32" fill="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+        <path fill="currentColor" d="M16.03 3.2A12.7 12.7 0 0 0 5.2 22.5L3.8 28.8l6.42-1.34A12.73 12.73 0 1 0 16.03 3.2Zm0 22.96c-1.96 0-3.78-.56-5.33-1.54l-.38-.24-3.8.8.8-3.7-.25-.39a10.25 10.25 0 1 1 8.96 5.07Zm5.78-7.67c-.31-.16-1.86-.92-2.15-1.03-.29-.1-.5-.16-.71.16-.21.31-.82 1.03-1 1.24-.18.21-.37.24-.69.08-.31-.16-1.33-.49-2.54-1.56-.94-.84-1.57-1.87-1.75-2.18-.18-.31-.02-.48.14-.64.14-.14.31-.37.47-.55.16-.18.21-.31.31-.52.1-.21.05-.39-.03-.55-.08-.16-.71-1.7-.97-2.33-.26-.61-.52-.53-.71-.54h-.6c-.21 0-.55.08-.84.39-.29.31-1.1 1.08-1.1 2.62s1.13 3.04 1.29 3.25c.16.21 2.22 3.39 5.38 4.75.75.32 1.34.51 1.8.65.76.24 1.45.2 2 .12.61-.09 1.86-.76 2.13-1.49.26-.73.26-1.36.18-1.49-.08-.13-.29-.21-.6-.37Z" />
+      </svg>
+    </a>
+  );
+};
+
+export default FloatingWhatsApp;
